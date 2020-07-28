c4f3bc9707c4f45c2d15afd98f65aaa6a9fb9a37
Yixie Chen <44877158+ychen132@users.noreply.github.com>
2020-07-28 15:37:55 -0400

    Add files via upload

---
+++ SelectionSort/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = {20,35,-15,7,55,1,-22};
+        for(int lastUnsortedIndex = intArray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--){
+            int largest = 0;
+            for(int i =1;i<=lastUnsortedIndex;i++){
+                if(intArray[i]>intArray[largest]){
+                    largest = i;
+                }
+            } 
+            swap(intArray,largest,lastUnsortedIndex);
+        }
+
+
+        for(int i:intArray){
+            System.out.print(i+" ");
+        }
+    }
+
+
+    public static void swap(int[] array, int i, int j){
+        if(i==j){
+            return;
+        }else{
+            int temp;
+            temp = array[i];
+            array[i]=array[j];
+            array[j]= temp;
+        }
+    }
+}
